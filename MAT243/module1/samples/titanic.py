492bb45edce07a6ff859eb6399d0bacfcc157a73
fiv95 <frank4law@gmail.com>
2025-03-03 19:17:25 -0500

    updated git ignore to handle MAT243 files && added test titanic.py

--- MAT243/module1/samples/titanic.py
+++ MAT243/module1/samples/titanic.py
@@ -1,3 +1,18 @@
 import seaborn as sns
 
 titanic = sns.load_dataset("titanic")
+
+print(titanic.shape)
+print(titanic.columns)
+print()
+print(titanic.dtypes)
+
+# means of all numerical variables #
+
+print(titanic.select_dtypes(include=(float, int)).mean())
+print(titanic.select_dtypes(include=(float, int)).min())
+
+titanic[["sex", "survived"]].head()
+
+titanic[titanic.pclass == 3]
+titanic.describe()
